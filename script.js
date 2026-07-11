807a4d31ad72220d2ed46e6c0092ca4287230510
adnankinggahst-source <adnankinggahst@gmail.com>
2026-07-11 20:49:48 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("GtaBestMods Loaded");
+const searchBox = document.getElementById("search");
+
+if (searchBox) {
+    searchBox.addEventListener("input", function () {
+        console.log("Search:", this.value);
+    });
+}
